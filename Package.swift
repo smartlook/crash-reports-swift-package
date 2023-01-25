d3d962820fc1e32c2f4add99d1e83b918e6f3740
jCharv <m@jiricharvat.com>
2023-01-25 11:51:11 +0100

    Typo fixed.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Smartlook Crash Reports",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
         .tvOS(.v13)
     ],
     products: [
