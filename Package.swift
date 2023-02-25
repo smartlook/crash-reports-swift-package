ed9415e6d71596e9b3aeee5463aa7380aff1d4e4
Jiri Charvat <m@jiricharvat.com>
2023-02-25 16:30:14 +0100

    1.0.1.6

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SmartlookCrashReports",
-            url: "https://sdk.smartlook.com/ios/smartlook-ios-crashreports-1.0.1.5.zip",
-            checksum: "d2f376ea68e0158d05bd0c18708dfa5c689fc38281a9278f3de4b29268465aa8"
+            url: "https://sdk.smartlook.com/ios/smartlook-ios-crashreports-1.0.1.6.zip",
+            checksum: "6993d296eec69dc9002c2864d673466a44379ecc382e016b2135ef4a79140ff3"
         ),
     ]
 )
